10622d32d3773f2117defa946b6404ef24052752
Adrish <54068755+Adrish1999@users.noreply.github.com>
2020-09-07 21:56:03 +0530

    Create answer_one.py

---
+++ Analysis-3/answer_one.py
@@ -0,0 +1,87 @@
+# Question 1 (20%)
+# Load the energy data from the file `Energy Indicators.xls`, which is a list of indicators of [energy supply and renewable electricity production](Energy%20Indicators.xls) from the [United Nations](http://unstats.un.org/unsd/environment/excel_file_tables/2013/Energy%20Indicators.xls) for the year 2013, and should be put into a DataFrame with the variable name of **energy**.
+# 
+# Keep in mind that this is an Excel file, and not a comma separated values file. Also, make sure to exclude the footer and header information from the datafile. The first two columns are unneccessary, so you should get rid of them, and you should change the column labels so that the columns are:
+# 
+# `['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable']`
+# 
+# Convert `Energy Supply` to gigajoules (there are 1,000,000 gigajoules in a petajoule). For all countries which have missing data (e.g. data with "...") make sure this is reflected as `np.NaN` values.
+# 
+# Rename the following list of countries (for use in later questions):
+# 
+# ```"Republic of Korea": "South Korea",
+# "United States of America": "United States",
+# "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+# "China, Hong Kong Special Administrative Region": "Hong Kong"```
+# 
+# There are also several countries with numbers and/or parenthesis in their name. Be sure to remove these, 
+# 
+# e.g. 
+# 
+# `'Bolivia (Plurinational State of)'` should be `'Bolivia'`, 
+# 
+# `'Switzerland17'` should be `'Switzerland'`.
+# 
+# <br>
+# 
+# Next, load the GDP data from the file `world_bank.csv`, which is a csv containing countries' GDP from 1960 to 2015 from [World Bank](http://data.worldbank.org/indicator/NY.GDP.MKTP.CD). Call this DataFrame **GDP**. 
+# 
+# Make sure to skip the header, and rename the following list of countries:
+# 
+# ```"Korea, Rep.": "South Korea", 
+# "Iran, Islamic Rep.": "Iran",
+# "Hong Kong SAR, China": "Hong Kong"```
+# 
+# <br>
+# 
+# Finally, load the [Sciamgo Journal and Country Rank data for Energy Engineering and Power Technology](http://www.scimagojr.com/countryrank.php?category=2102) from the file `scimagojr-3.xlsx`, which ranks countries based on their journal contributions in the aforementioned area. Call this DataFrame **ScimEn**.
+# 
+# Join the three datasets: GDP, Energy, and ScimEn into a new dataset (using the intersection of country names). Use only the last 10 years (2006-2015) of GDP data and only the top 15 countries by Scimagojr 'Rank' (Rank 1 through 15). 
+# 
+# The index of this DataFrame should be the name of the country, and the columns should be ['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations',
+#        'Citations per document', 'H index', 'Energy Supply',
+#        'Energy Supply per Capita', '% Renewable', '2006', '2007', '2008',
+#        '2009', '2010', '2011', '2012', '2013', '2014', '2015'].
+# 
+# *This function should return a DataFrame with 20 columns and 15 entries.*
+
+
+
+import pandas as pd
+import numpy as np
+def answer_one():
+    energy = pd.read_excel('Energy Indicators.xls')
+    energy = energy[16:243]
+    energy = energy.drop(energy.columns[[0, 1]], axis=1)
+    energy.rename(columns={'Environmental Indicators: Energy': 'Country','Unnamed: 3':'Energy Supply','Unnamed: 4':'Energy Supply per Capita','Unnamed: 5':'% Renewable'}, inplace=True)
+    energy.replace('...', np.nan,inplace = True)
+    energy['Energy Supply'] *= 1000000
+    def remove_digit(data):
+        newData = ''.join([i for i in data if not i.isdigit()])
+        i = newData.find('(')
+        if i>-1: newData = newData[:i]
+        return newData.strip()
+    energy['Country'] = energy['Country'].apply(remove_digit)
+    di = {"Republic of Korea": "South Korea",
+    "United States of America": "United States",
+    "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+    "China, Hong Kong Special Administrative Region": "Hong Kong"}
+    energy.replace({"Country": di},inplace = True)
+    
+    ###################################################################################
+    GDP = pd.read_csv('world_bank.csv', skiprows=4)
+    GDP.replace({"Korea, Rep.": "South Korea", 
+                "Iran, Islamic Rep.": "Iran",
+                "Hong Kong SAR, China": "Hong Kong"}, inplace=True)
+    GDP.rename(columns={'Country Name': 'Country'}, inplace=True)
+    ScimEn = pd.read_excel('scimagojr-3.xlsx')
+
+    df = pd.merge(pd.merge(energy, GDP, on='Country'), ScimEn, on='Country')
+    df.set_index('Country',inplace=True)
+    df = df[['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'Citations per document', 'H index', 'Energy Supply', 'Energy Supply per Capita', '% Renewable', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']]
+    df = (df.loc[df['Rank'].isin([i for i in range(1, 16)])])
+    df.sort('Rank',inplace=True)
+    return df
+
+answer_one()
+
